572b08456ddccf30f1691f0adb905563d6704ba3
Jay Bienvenu <bienvenunet@yahoo.com>
2015-12-12 10:48:07 -0600

    Fix: Strip '.txt' extensions from property labels.

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -1,10 +1,10 @@
 echo -n "{"
 delimiter2=""
-ls -1 $1*.txt | while read -r filename
+ls -1 $1*.txt | cut -d'.' -f 1 | while read -r filename
   do
   delimiter1=""
   echo -n "$delimiter2'$filename':["
-  cat $filename | while read -r word
+  cat $filename.txt | while read -r word
     do
     echo -n "$delimiter1'$word'"
     delimiter1=","
