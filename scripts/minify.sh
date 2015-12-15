af2d28163f2170a4bc859323c80687426974ad48
Jay Bienvenu <bienvenunet@yahoo.com>
2015-12-14 21:42:16 -0600

    Fix quotes.

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -3,10 +3,10 @@ delimiter2=""
 ls -1 $1*.txt | cut -d'.' -f 1 | while read -r filename
   do
   delimiter1=""
-  echo -n "$delimiter2'$filename':["
+  echo -n "$delimiter2\"$filename\":["
   cat $filename.txt | while read -r word
     do
-    echo -n "$delimiter1'$word'"
+    echo -n "$delimiter1\"$word\""
     delimiter1=","
     done
   echo -n "]"
